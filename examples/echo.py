f530c9b40b50a9b2672702bbdbd333c7b0e028a0
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-21 14:31:49 -0800

    Added echo server example.

---
+++ examples/echo.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+import logging
+
+from functools import partial
+
+from marrow.server.base import Server
+from marrow.server.protocol import Protocol
+
+
+log = logging.getLogger(__name__)
+
+
+
+class EchoProtocol(Protocol):
+    def accept(self, client):
+        log.info("Accepted connection from %r.", client.address)
+        
+        client.write("Hello!  Type something and press enter.  Type /quit to quit.\n")
+        client.read_until("\r\n", partial(self.on_line, client))
+    
+    def on_line(self, client, data):
+        if data[:-2] == "/quit":
+            client.write("Goodbye!\r\n", client.close)
+            return
+        
+        client.write(data)
+        client.read_until("\r\n", partial(self.on_line, client))
+
+
+
+if __name__ == '__main__':
+    import logging
+    
+    logging.basicConfig(level=logging.INFO)
+    
+    # For multi-process add "fork=" and a non-zero value.
+    # Zero or None auto-detect the logical processors.
+    Server(None, 8000, EchoProtocol).start()
