734347ab3f299fb30ec02f2f79da066d832735ba
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-22 06:24:57 -0800

    Bytes updates.

--- examples/echo.py
+++ examples/echo.py
@@ -14,18 +14,18 @@ log = logging.getLogger(__name__)
 
 class EchoProtocol(Protocol):
     def accept(self, client):
-        log.info("Accepted connection from %r.", client.address)
+        log.info(b"Accepted connection from %r.", client.address)
         
-        client.write("Hello!  Type something and press enter.  Type /quit to quit.\n")
+        client.write(b"Hello!  Type something and press enter.  Type /quit to quit.\n")
         client.read_until("\r\n", partial(self.on_line, client))
     
     def on_line(self, client, data):
-        if data[:-2] == "/quit":
-            client.write("Goodbye!\r\n", client.close)
+        if data[:-2] == b"/quit":
+            client.write(b"Goodbye!\r\n", client.close)
             return
         
         client.write(data)
-        client.read_until("\r\n", partial(self.on_line, client))
+        client.read_until(b"\r\n", partial(self.on_line, client))
 
 
 
