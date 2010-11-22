3104ab9271448a6a87d8f3a8372363f38912ba6f
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-22 06:26:50 -0800

    Missed one bytes reference.

--- examples/echo.py
+++ examples/echo.py
@@ -17,7 +17,7 @@ class EchoProtocol(Protocol):
         log.info(b"Accepted connection from %r.", client.address)
         
         client.write(b"Hello!  Type something and press enter.  Type /quit to quit.\n")
-        client.read_until("\r\n", partial(self.on_line, client))
+        client.read_until(b"\r\n", partial(self.on_line, client))
     
     def on_line(self, client, data):
         if data[:-2] == b"/quit":
